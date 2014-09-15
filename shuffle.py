9f4c72364bf17f3698420a54ca31d4a37d4bad03
coffeant <coffeant@gmail.com>
2014-09-15 15:15:57 +0800

    Create shuffle.py

---
+++ shuffle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+#-*- encoding : utf8 -*-#
+
+import os
+import sys 
+import random
+
+def shuffle(source, to):
+    with open('./nameinfo.txt','r') as source:
+        data = [ (random.random(), line) for line in source ]
+    data.sort()
+    with open('./nameinfo_shuffle.txt','w') as target:
+        for _, line in data:
+            target.write(line)
+
+
+if __name__ == '__main__':
+    _from = 'nameinfo.txt'
+    _to = 'nameinfo_shuffle.txt'
+    shuffle(_from , _to)
